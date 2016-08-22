6eff9b12d958815a355aef3bc47e993f42260937
Paulius Rimavicius <paulius.rimavicius@gmail.com>
2016-08-22 19:33:35 +0300

    Script changed

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
 
     finish: {
         receive: (bot, message) => {
-            return bot.say('').then(() => 'speak');
+            return bot.say('finish').then(() => 'speak');
             /*return bot.getProp('name')
                 .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
                         'teach me how to do anything else!'))
