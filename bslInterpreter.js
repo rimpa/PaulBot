562c7c97d7b49ed66a95b9a151d555e34e6ed77f
Paulius Rimavicius <paulius.rimavicius@gmail.com>
2016-08-29 22:37:58 +0300

    Added Interpreter class 8

--- bslInterpreter.js
+++ bslInterpreter.js
@@ -39,12 +39,81 @@ class BslInterpreter {
                 this.setStep(0);
                 break;
             }
-            next = this.execStetement(message);
+            next = this.execStetement(message, statementJson);
             message = '';
             if (next) {
                 this.increaseStep();
             }
         }
+    }
+
+    execStetement(message, statement) {
+      //console.log(statement);
+      if (typeof statement === 'undefined') { return false; }
+      if (typeof statement.statement === 'undefined') { return false; }
+
+      switch (statement.statement) {
+        case "SAY":
+            var randMess = this._getRandomArrayValue(statement.body);
+            if (typeof randMess.value !== 'undefined') {
+                this.say(randMess.value);
+            }
+            return true;
+            break;
+        /*case "ASK":
+            if (typeof sess.asked !== 'undefined' && sess.asked) {
+              // collect && validate
+              if (msg) {
+                var collectedValue = validator.trim(msg);
+                var collectedVariable = statement.body.collect.body.value.value;
+                if (typeof statement.body.validate !== 'undefined') {
+                  for (var i = 0, len = statement.body.validate.length; i < len; i++) {
+                    var validate = statement.body.validate[i].body.value;
+                    var funcName = validate.function.value;
+                    var validateFunction = null;
+                    try {
+                      var validateFunction = validator[funcName];// require('./validate/'+ funcName +'.js');
+                    } catch (ex) {
+                      io.emit('chat message', 'Validation function "'+funcName+'" not found.');
+                    }
+                    if (validateFunction) {
+                      //doValidate(validateFunction, )
+                      if (!validateFunction.validate(getParams(validate.params))) {
+                        var randError = getRandomArrayValue(validate.error);
+                        var errText = getRandomArrayValue(randError.body).value;
+                        io.emit('chat message', errText);
+                        return false;
+                      }
+                    }
+                    var randError = getRandomArrayValue(validate.error);
+                    var errText = getRandomArrayValue(randError.body)
+                  }
+                  sess.asked = false;
+                  return true;
+                } else {
+                  sess.collected_value = collectedValue;
+                  sess.collected_variable = collectedVariable;
+                  addVariable(collectedVariable, collectedValue, sess);
+                  sess.asked = false;
+                  return true;
+                }
+              }
+            } else {
+              var randMess = getRandomArrayValue(statement.body.ask);
+              if (typeof randMess.value !== 'undefined') {
+                io.emit('chat message', randMess.value);
+              }
+            }
+            sess.asked = true;
+            return false;
+            break;
+        case "PLUGIN":
+            console.log("Plugin");
+            return true;
+            break;*/
+      }
+
+      return false;
     }
 
     getScenario(message) {
@@ -64,7 +133,6 @@ class BslInterpreter {
     }
 
     getStatement() {
-        console.log(this.programJson);
         if (this.scenario === 'main_scenario') {
             if (typeof this.programJson.main.body[this.step] !== 'undefined') {
                 return this.programJson.main.body[this.step];
