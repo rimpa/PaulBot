7214b4d0b0251310acd893b1c879a98d93ded6ea
Paulius Rimavicius <paulius.rimavicius@gmail.com>
2016-08-29 20:23:51 +0300

    Added Interpreter class 3

--- bslInterpreter.js
+++ bslInterpreter.js
@@ -5,16 +5,24 @@ class BslInterpreter {
         options = options || {};
         //if (!options.store || !options.lock || !options.userId) {
         if (!options.bot) {
-            throw new Error('Invalid arguments. bot is required');
+            throw new Error('Invalid arguments. bot and store are required');
+        }
+
+        if (typeof this.bot.getProp('scenario') === 'undefined') {
+            this.bot.setProp('scenario','main_scenario');
+        }
+
+        if (typeof this.bot.getProp('step') === 'undefined') {
+            this.bot.setProp('step',0);
         }
 
         this.bot = options.bot;
     }
 
     say(text) {
-        this.bot.say(text);
+        this.bot.say(text + this.bot.getProp('scenario'));
     }
-    
+
 }
 
 module.exports = BslInterpreter;
