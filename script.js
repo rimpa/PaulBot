329b1036db84ad9610295a50045e5ab2447044dd
Paulius Rimavicius <paulius.rimavicius@gmail.com>
2016-09-18 15:17:53 +0300

    fix start bug

--- script.js
+++ script.js
@@ -6,6 +6,29 @@ const BslInterpreter = require('./bslInterpreter');
 const Script = require('smooch-bot').Script;
 const fs = require('fs');
 
+var processSpeak = function(bot, message) {
+  // compile bsl script
+  let dirname = __dirname;
+  let scenarioJson = {};
+  let bslSource = fs.readFileSync(__dirname + '/script.bsl', 'utf8');
+  try {
+      scenarioJson = BslParser.parse(bslSource);
+  }
+  catch(err) {
+      //var message = err.message + ' on Line:'+err.location.start.line+' column:'+err.location.start.column;
+      scenarioJson = {};
+  }
+  var bslJsonString = JSON.stringify({ program: scenarioJson });
+  // TODO: cache to file ?
+  //var programJson = { program: scenarioJson };
+  // end compile bsl script
+
+  var bslInterpreter = new BslInterpreter({'bot':bot, 'programJson': scenarioJson});
+
+  bslInterpreter.startInterpret(message.text.trim());
+  return 'speak';
+}
+
 module.exports = new Script({
     processing: {
         //prompt: (bot) => bot.say('Beep boop...'),
@@ -14,31 +37,14 @@ module.exports = new Script({
 
     start: {
       receive: (bot, message) => {
-          // compile bsl script
-          let dirname = __dirname;
-          let scenarioJson = {};
-          let bslSource = fs.readFileSync(__dirname + '/script.bsl', 'utf8');
-          try {
-              scenarioJson = BslParser.parse(bslSource);
-          }
-          catch(err) {
-              //var message = err.message + ' on Line:'+err.location.start.line+' column:'+err.location.start.column;
-              scenarioJson = {};
-          }
-          var bslJsonString = JSON.stringify({ program: scenarioJson });
-          // TODO: cache to file ?
-          //var programJson = { program: scenarioJson };
-          // end compile bsl script
-
-          var bslInterpreter = new BslInterpreter({'bot':bot, 'programJson': scenarioJson});
-
-          bslInterpreter.startInterpret(message.text.trim());
-          return 'speak';
+        processSpeak(bot, message);
       }
     },
 
     speak: {
         receive: (bot, message) => {
+          processSpeak(bot, message);
+          /*
             // compile bsl script
             let dirname = __dirname;
             let scenarioJson = {};
@@ -58,18 +64,7 @@ module.exports = new Script({
             var bslInterpreter = new BslInterpreter({'bot':bot, 'programJson': scenarioJson});
 
             bslInterpreter.startInterpret(message.text.trim());
-            return 'speak';
-        }
-    },
-
-    askName: {
-        prompt: (bot) => bot.say('What\'s your name?'),
-        receive: (bot, message) => {
-            const name = message.text;
-            return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}
-Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
-                .then(() => 'finish');
+            return 'speak';*/
         }
     },
 
