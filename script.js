0d79bb3612eff85e5034a2de42499e99097dec42
Paulius Rimavicius <paulius.rimavicius@gmail.com>
2016-09-18 15:12:28 +0300

    fix start bug

--- script.js
+++ script.js
@@ -13,7 +13,28 @@ module.exports = new Script({
     },
 
     start: {
-        receive: () => 'speak'
+      receive: (bot, message) => {
+          // compile bsl script
+          let dirname = __dirname;
+          let scenarioJson = {};
+          let bslSource = fs.readFileSync(__dirname + '/script.bsl', 'utf8');
+          try {
+              scenarioJson = BslParser.parse(bslSource);
+          }
+          catch(err) {
+              //var message = err.message + ' on Line:'+err.location.start.line+' column:'+err.location.start.column;
+              scenarioJson = {};
+          }
+          var bslJsonString = JSON.stringify({ program: scenarioJson });
+          // TODO: cache to file ?
+          //var programJson = { program: scenarioJson };
+          // end compile bsl script
+
+          var bslInterpreter = new BslInterpreter({'bot':bot, 'programJson': scenarioJson});
+
+          bslInterpreter.startInterpret(message.text.trim());
+          return 'speak';
+      }
     },
 
     speak: {
@@ -37,7 +58,6 @@ module.exports = new Script({
             var bslInterpreter = new BslInterpreter({'bot':bot, 'programJson': scenarioJson});
 
             bslInterpreter.startInterpret(message.text.trim());
-            //return bot.getProp('name').then(() => 'speak');
             return 'speak';
         }
     },
